7f64c558fa62f25d8bf46f730712863442d68877
Daniel <dangarflo12@gmail.com>
2025-12-09 08:52:49 +0100

    dsdsd

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Base path for GitHub Pages. Replace with your repo name if different.
+  base: '/TareaVueDAW/',
   define: {
     'process.env': process.env
   },
